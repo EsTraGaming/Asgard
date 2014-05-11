8d2d25da0610750d49d83fec402a12ed602f533b
Ferko <alex.dangtran@gmail.com>
2014-05-10 22:43:37 -0500

    Update classpath for correct paths. Add gitignore file. Update main stub with template code from CodeRunner project.

--- src/com/me/TacticsGame/main.java
+++ src/com/me/TacticsGame/main.java
@@ -1,10 +1,21 @@
 package com.me.TacticsGame;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.width = 900;
+        cfg.height = 700;
+        cfg.vSyncEnabled = true;
+        cfg.fullscreen = false;
+        cfg.useGL20 = true;
+        cfg.title = "Tactics Game";
+        
+        new LwjglApplication(new TacticsGame(), cfg);
+    }
 
 }
